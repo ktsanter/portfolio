8be9b70503f62854cefc7dd7e68397c4a701ccf2
SpunkyAardvark123 <ktsanter@gmail.com>
2024-01-20 08:51:08 -0600

    box model project, v1 complete

--- box model project/the box model (web)/story_content/user.js
+++ box model project/the box model (web)/story_content/user.js
@@ -2,14 +2,17 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6gsmXPoHWXb":
+      case "6VsrC0uta2c":
         Script1();
         break;
-      case "6OpotVs7DAB":
+      case "67lDcbVqOHb":
         Script2();
         break;
-      case "6nODroLpQRt":
+      case "5k3v4RRNrvQ":
         Script3();
+        break;
+      case "5xW0qMRGy5M":
+        Script4();
         break;
   }
 }
@@ -24,6 +27,11 @@ const margin = player.GetVar("sliderMargin");
 const stateName = "state-" + padding + "-" + border + "-" + margin;
 player.SetVar("boxStateName", stateName);
 
+const boxWidth = player.GetVar("contentWidth") + 2 * padding + 2 * border + 2 * margin;
+const boxHeight = player.GetVar("contentHeight") + 2 * padding + 2 * border + 2 * margin;
+player.SetVar("totalWidth", boxWidth);
+player.SetVar("totalHeight", boxHeight);
+
 }
 
 function Script2()
@@ -36,6 +44,11 @@ const margin = player.GetVar("sliderMargin");
 const stateName = "state-" + padding + "-" + border + "-" + margin;
 player.SetVar("boxStateName", stateName);
 
+const boxWidth = player.GetVar("contentWidth") + 2 * padding + 2 * border + 2 * margin;
+const boxHeight = player.GetVar("contentHeight") + 2 * padding + 2 * border + 2 * margin;
+player.SetVar("totalWidth", boxWidth);
+player.SetVar("totalHeight", boxHeight);
+
 }
 
 function Script3()
@@ -47,6 +60,28 @@ const margin = player.GetVar("sliderMargin");
 
 const stateName = "state-" + padding + "-" + border + "-" + margin;
 player.SetVar("boxStateName", stateName);
+
+const boxWidth = player.GetVar("contentWidth") + 2 * padding + 2 * border + 2 * margin;
+const boxHeight = player.GetVar("contentHeight") + 2 * padding + 2 * border + 2 * margin;
+player.SetVar("totalWidth", boxWidth);
+player.SetVar("totalHeight", boxHeight);
+
+}
+
+function Script4()
+{
+  const player = GetPlayer();
+const padding = player.GetVar("sliderPadding");
+const border = player.GetVar("sliderBorder");
+const margin = player.GetVar("sliderMargin");
+
+const stateName = "state-" + padding + "-" + border + "-" + margin;
+player.SetVar("boxStateName", stateName);
+
+const boxWidth = player.GetVar("contentWidth") + 2 * padding + 2 * border + 2 * margin;
+const boxHeight = player.GetVar("contentHeight") + 2 * padding + 2 * border + 2 * margin;
+player.SetVar("totalWidth", boxWidth);
+player.SetVar("totalHeight", boxHeight);
 
 }
 
