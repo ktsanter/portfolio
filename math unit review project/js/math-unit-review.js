76da18b2e226c4bf87a523aac7a93cdeec129819
SpunkyAardvark123 <ktsanter@gmail.com>
2024-01-21 09:36:47 -0600

    math unit review project: midway through first draft

--- math unit review project/js/math-unit-review.js
+++ math unit review project/js/math-unit-review.js
@@ -7,7 +7,7 @@
 //--------------------------------------------------------------------------------
 class MathUnitReview {
   static questionInfo = null;
-  static numQuestions = -1;
+  //static numQuestions = -1;
   static articulateVars = null;
   
   constructor() {
@@ -19,7 +19,6 @@ class MathUnitReview {
   //------------------------------------------------------------------------------
   static initialize(initInfo)
   {
-    console.log("MathUnitReview.initialize");
     const player = GetPlayer();
     
     let questionInfo = {};
@@ -29,6 +28,7 @@ class MathUnitReview {
     
     let vars = {};
     vars.currentQuestionNumber = initInfo.currentQuestionNumber;
+    vars.currentQuestionSelection = initInfo.currentQuestionSelection;
     vars.questionNumberLabel = initInfo.questionNumberLabel;
     vars.questionStem = initInfo.questionStem;
     vars.response = [
@@ -36,8 +36,8 @@ class MathUnitReview {
       initInfo.responseB,
       initInfo.responseC,
       initInfo.responseD,
-    ]
-    
+    ];
+ 
     MathUnitReview.articulateVars = vars;
     MathUnitReview.questionInfo = questionInfo
     
@@ -46,8 +46,6 @@ class MathUnitReview {
   
   static loadQuestion()
   {
-    console.log("MathUnitReview.loadQuestion");
-    
     const player = GetPlayer();
     const vars = MathUnitReview.articulateVars;
     const qInfo = MathUnitReview.questionInfo;
@@ -63,11 +61,52 @@ class MathUnitReview {
     
     player.SetVar(vars.questionNumberLabel, currentQuestionNum);
     player.SetVar(vars.questionStem, question.stem);
+    player.SetVar(vars.currentQuestionSelection, question.selection);
     
     const responses = question.response;
     for (let i = 0; i < responses.length; i++) {
       player.SetVar(vars.response[i], responses[i]);
     }
+  }
+  
+  static setResponseSelection(selectionValue)
+  {
+    const player = GetPlayer();
+    const vars = MathUnitReview.articulateVars;
+    const qInfo = MathUnitReview.questionInfo;
+
+    const currentQuestionNum = player.GetVar(vars.currentQuestionNumber);
+    qInfo.question[currentQuestionNum - 1].selection = selectionValue;
+    
+    player.SetVar(vars.currentQuestionSelection, selectionValue);
+  }
+  
+  static changeQuestion(mode)
+  {
+    const player = GetPlayer();
+    const vars = MathUnitReview.articulateVars;
+    const qInfo = MathUnitReview.questionInfo;
+    
+    const currentQuestionNum = player.GetVar(vars.currentQuestionNumber);
+    const numQuestions = MathUnitReview.questionInfo.numQuestions;
+
+    let newQuestionNum;
+    if (mode == "next") {
+      if (currentQuestionNum >= numQuestions) {
+        console.log("WARNING: MathUnitReview.nextQuestion called with 'nextd' but no more questions");
+        return;
+      }
+      newQuestionNum = currentQuestionNum + 1;
+      
+    } else if (mode == "previous") {
+      if (currentQuestionNum <= 1) {
+        console.log("WARNING: MathUnitReview.nextQuestion called with 'previous' at beginning of list");
+        return;
+      }
+      newQuestionNum = currentQuestionNum - 1;
+    }
+    
+    player.SetVar(vars.currentQuestionNumber, newQuestionNum);
   }
   
   static checkAnswers()
@@ -92,6 +131,7 @@ class MathUnitReview {
       for (let j = 0; j < nResponses; j++) {
         question.response.push("response " + (i + 1) + letterMap.charAt(j));
       }
+      question.selection = -1;
       
       qInfo.question.push(question);
     }
--- math unit review project/math unit review (web)/story_content/user.js
+++ math unit review project/math unit review (web)/story_content/user.js
@@ -2,14 +2,44 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6gYKngH3U1L":
+      case "6kibWkI5zT6":
         Script1();
         break;
-      case "62Y1w5VUlQI":
+      case "6megqC6t5uK":
         Script2();
         break;
-      case "5dsAv89zgMo":
+      case "6Wa1kMLSOj8":
         Script3();
+        break;
+      case "6ktdntqqNbY":
+        Script4();
+        break;
+      case "5rLBpvqUgPn":
+        Script5();
+        break;
+      case "6hC8A5Jhf2v":
+        Script6();
+        break;
+      case "63uaHr6RmoX":
+        Script7();
+        break;
+      case "6XbANQIcbaj":
+        Script8();
+        break;
+      case "5cM5tLSom5x":
+        Script9();
+        break;
+      case "5oyF7KRA9Nt":
+        Script10();
+        break;
+      case "6nvopdAJ6F0":
+        Script11();
+        break;
+      case "5jn106mDd57":
+        Script12();
+        break;
+      case "6pYUKHJhHxP":
+        Script13();
         break;
   }
 }
@@ -19,6 +49,7 @@ function Script1()
   MathUnitReview.initialize({
 	"numQuestions": "numQuestions",
 	"currentQuestionNumber": "currentQuestionNumber",
+	"currentQuestionSelection": "currentQuestionSelection",
 	"questionNumberLabel": "questionNumberLabel",
 	"questionStem": "questionStem",
 	"responseA": "responseA",
@@ -35,6 +66,56 @@ function Script2()
 }
 
 function Script3()
+{
+  MathUnitReview.changeQuestion("next");
+}
+
+function Script4()
+{
+  MathUnitReview.changeQuestion("previous");
+}
+
+function Script5()
+{
+  MathUnitReview.setResponseSelection(4);
+}
+
+function Script6()
+{
+  MathUnitReview.setResponseSelection(4);
+}
+
+function Script7()
+{
+  MathUnitReview.setResponseSelection(3);
+}
+
+function Script8()
+{
+  MathUnitReview.setResponseSelection(3);
+}
+
+function Script9()
+{
+  MathUnitReview.setResponseSelection(1);
+}
+
+function Script10()
+{
+  MathUnitReview.setResponseSelection(1);
+}
+
+function Script11()
+{
+  MathUnitReview.setResponseSelection(2);
+}
+
+function Script12()
+{
+  MathUnitReview.setResponseSelection(2);
+}
+
+function Script13()
 {
   MathUnitReview.checkAnswers();
 }
